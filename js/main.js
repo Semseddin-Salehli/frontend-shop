117fe502be8cab75baff50bcce6d9d8b85441e8a
Semseddin-Salehli <semseddinsalehli1@gmail.com>
2022-05-04 12:25:47 +0400

    changed for username text

--- js/main.js
+++ js/main.js
@@ -2,16 +2,17 @@ $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
 
-if(sessionStorage.getItem(btoa('auth')) !=undefined) {
-    $('.navLoginBtn').css('display' , 'none');
-    $('.navExitBtn').css('display' , 'block');
+if (sessionStorage.getItem(btoa('auth')) != undefined) {
+    $('.navLoginBtn').css('display', 'none');
+    $('.navExitBtn').css('display', 'block');
 
     let username = atob(sessionStorage.getItem(btoa('auth')).split(' ')[1]).split(':')[0]
 
-    $('.usernameText').html(username);
+    $('.usernameText').html(`<span class="badge badge-dark badge-xlg ">İstifadəçi adı :</span>
+     <span class="text-success">${username}</span>`);
 } else {
-    $('.navLoginBtn').css('display' , 'block');
-    $('.navExitBtn').css('display' , 'none');
+    $('.navLoginBtn').css('display', 'block');
+    $('.navExitBtn').css('display', 'none');
 }
 
 function exit() {
